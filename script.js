8315ce0deb21902c8a54ff8f96204fd857576324
toskr <dccconnect@gmail.com>
2022-10-17 21:15:41 +0100

    form data added to array

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(author, title, pages, read){
+    const obj = new Book(title, author, pages, read);
+    myLibrary.push(obj)
 }
+
+
+const add = document.querySelector('button');
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const read = document.getElementById('read');
+const pages = document.getElementById('pages');
+
+add.addEventListener('click', ()=>{
+    addBookToLibrary(author.value, title.value, pages.value, read.checked);
+    console.log(myLibrary);    
+})
+
+
