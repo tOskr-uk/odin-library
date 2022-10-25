d23022137745d5725c86626df5a65fa890d0f3d2
toskr <dccconnect@gmail.com>
2022-10-25 17:17:26 +0100

    read / unread object value presented as a button in the table

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const myLibrary = [];
-const add = document.getElementById('add-book');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
-const read = document.getElementById('read');
+const addBook = document.getElementById('add-book');
 const library = document.querySelector('.library');
+let btnCollDel;
 
+
+// adds new books to the myLibrary array
 function addBookToLibrary(title, author, pages, read){
     const obj = new Book(title, author, pages, read);
     myLibrary.push(obj);
 }
 
-add.addEventListener('click', ()=>{
+// event listener for adding a book button press
+addBook.addEventListener('click', ()=>{
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const read = document.getElementById('read');
+
     addBookToLibrary(title.value, author.value, pages.value, read.checked);
     clearTable();
     buildTable();
+})
+
+// event listener for deleting a book button press
+library.addEventListener('click', e=>{
+    if(e.target.classList.contains('book-del')){
+        const arrEl = e.target.getAttribute('data');
+        myLibrary.splice(arrEl,1);
+        clearTable();
+        buildTable();
+    }
 })
 
 // clears the html table
@@ -22,19 +37,20 @@ function clearTable(){
     library.innerHTML='';
 }
 
+// builds and populates the html table
 function buildTable(){
     // build table head
-    const t = document.createElement('table');
-    t.classList.add('table');
+    const tbl = document.createElement('table');
+    tbl.classList.add('table');
     let tr = document.createElement('tr');
     let td = ['Title', 'Author', 'Pages', 'Read', 'Delete'];
     td.forEach(e=>{
         const th = document.createElement('th');
         th.innerText = e;
         tr.appendChild(th);
-    })
-    t.appendChild(tr);
-    library.appendChild(t);
+    });
+    tbl.appendChild(tr);
+    library.appendChild(tbl);
 
     // populate the table with myLibrary array data
     for(let i=0; i<myLibrary.length; i++){
@@ -42,34 +58,44 @@ function buildTable(){
         tr = document.createElement('tr');
         for(key in obj){
             td = document.createElement('td');
-            td.innerText=obj[key];
-            tr.appendChild(td);
+            if(key === 'read'){
+                let a = document.createElement('a');
+                console.log(obj[key]);
+                if(obj[key] == true){
+                    a.classList.add('btn', 'btn--blue');
+                    a.innerText='Read';
+                } else {
+                    a.classList.add('btn', 'btn--grey');
+                    a.innerText='Unread';
+                }
+                td.appendChild(a);
+            } else {
+                td.innerText=obj[key];
+            }
+            tr.appendChild(td);            
         }
 
         // add delete button
         let a = document.createElement('a');
-        a.classList.add('btn');
+        a.classList.add('btn', 'book-del');
         a.innerText='Delete';
+        a.setAttribute('data', i);
         td = document.createElement('td');
         td.appendChild(a);
         tr.appendChild(td);
-        t.appendChild(tr);
+        tbl.appendChild(tr);
     }
+    btnCollDel = document.querySelectorAll('.book-del')
+    // console.log(btnCollDel);
 }
 
 // generates some non specific data for the myLibrary array
 function generateSomeData(){
-    const obj1 = new Book('The Sword of Shannara', 'Terry Brooks', 759, false);
+    const obj1 = new Book('The Sword of Shannara', 'Terry Brooks', 759, true);
     myLibrary.push(obj1)
     const obj2 = new Book('The Elfstones of Shannara', 'Terry Brooks', 906, false);
     myLibrary.push(obj2)
 }
-
-generateSomeData();
-buildTable();
-
-
-
 
 // book constructor
 function Book(title, author, pages, read) {
@@ -78,3 +104,15 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+generateSomeData();
+buildTable();
+
+function getKey(){
+    // console.log(myLibrary[0]);
+    for(key in myLibrary[0]){
+        // console.log(key);
+    }
+}
+
+getKey()
